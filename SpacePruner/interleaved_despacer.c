fd304443a27b3642076ee83f7e819308c79d71be
Derek Ledbetter <derek.ledbetter.0127@gmail.com>
2017-07-09 16:43:27 -0700

    Now does almost everything with quadwords.

--- SpacePruner/interleaved_despacer.c
+++ SpacePruner/interleaved_despacer.c
@@ -16,43 +16,43 @@
 #if __ARM_NEON
 #include <arm_neon.h>
 
-static inline void transpose_in_place_8(uint8x8_t* restrict v0, uint8x8_t* restrict v1) {
-  uint8x8x2_t result = vtrn_u8(*v0, *v1);
+static inline void transpose_in_place_q_8(uint8x16_t* restrict v0, uint8x16_t* restrict v1) {
+  uint8x16x2_t result = vtrnq_u8(*v0, *v1);
   *v0 = result.val[0];
   *v1 = result.val[1];
 }
 
-static inline void transpose_in_place_16(uint8x8_t* restrict v0, uint8x8_t* restrict v1) {
-  uint16x4x2_t result = vtrn_u16(vreinterpret_u16_u8(*v0), vreinterpret_u16_u8(*v1));
-  *v0 = vreinterpret_u8_u16(result.val[0]);
-  *v1 = vreinterpret_u8_u16(result.val[1]);
+static inline void transpose_in_place_q_16(uint8x16_t* restrict v0, uint8x16_t* restrict v1) {
+  uint16x8x2_t result = vtrnq_u16(vreinterpretq_u16_u8(*v0), vreinterpretq_u16_u8(*v1));
+  *v0 = vreinterpretq_u8_u16(result.val[0]);
+  *v1 = vreinterpretq_u8_u16(result.val[1]);
 }
 
-static inline void transpose_in_place_32(uint8x8_t* restrict v0, uint8x8_t* restrict v1) {
-  uint32x2x2_t result = vtrn_u32(vreinterpret_u32_u8(*v0), vreinterpret_u32_u8(*v1));
-  *v0 = vreinterpret_u8_u32(result.val[0]);
-  *v1 = vreinterpret_u8_u32(result.val[1]);
+static inline void transpose_in_place_q_32(uint8x16_t* restrict v0, uint8x16_t* restrict v1) {
+  uint32x4x2_t result = vtrnq_u32(vreinterpretq_u32_u8(*v0), vreinterpretq_u32_u8(*v1));
+  *v0 = vreinterpretq_u8_u32(result.val[0]);
+  *v1 = vreinterpretq_u8_u32(result.val[1]);
 }
 
-static inline void transpose_8x8(uint8x8_t vectors[8]) {
-  transpose_in_place_8(&vectors[0], &vectors[1]);
-  transpose_in_place_8(&vectors[2], &vectors[3]);
-  transpose_in_place_8(&vectors[4], &vectors[5]);
-  transpose_in_place_8(&vectors[6], &vectors[7]);
-  transpose_in_place_16(&vectors[0], &vectors[2]);
-  transpose_in_place_16(&vectors[1], &vectors[3]);
-  transpose_in_place_16(&vectors[4], &vectors[6]);
-  transpose_in_place_16(&vectors[5], &vectors[7]);
-  transpose_in_place_32(&vectors[0], &vectors[4]);
-  transpose_in_place_32(&vectors[1], &vectors[5]);
-  transpose_in_place_32(&vectors[2], &vectors[6]);
-  transpose_in_place_32(&vectors[3], &vectors[7]);
+static inline void transpose_q_8x8(uint8x16_t vectors[8]) {
+  transpose_in_place_q_8(&vectors[0], &vectors[1]);
+  transpose_in_place_q_8(&vectors[2], &vectors[3]);
+  transpose_in_place_q_8(&vectors[4], &vectors[5]);
+  transpose_in_place_q_8(&vectors[6], &vectors[7]);
+  transpose_in_place_q_16(&vectors[0], &vectors[2]);
+  transpose_in_place_q_16(&vectors[1], &vectors[3]);
+  transpose_in_place_q_16(&vectors[4], &vectors[6]);
+  transpose_in_place_q_16(&vectors[5], &vectors[7]);
+  transpose_in_place_q_32(&vectors[0], &vectors[4]);
+  transpose_in_place_q_32(&vectors[1], &vectors[5]);
+  transpose_in_place_q_32(&vectors[2], &vectors[6]);
+  transpose_in_place_q_32(&vectors[3], &vectors[7]);
 }
 
 #define PRINT_8x8(var) ((void)printf("%s = %02X %02X %02X %02X  %02X %02X %02X %02X\n", #var, var[0], var[1], var[2], var[3], var[4], var[5], var[6], var[7]))
 
 size_t neon_interleaved_despace(char *bytes, size_t howmany) {
-  const size_t blockSize = 8 * 8;
+  const size_t blockSize = 8 * 16;
   const uint8_t space = 32;
 
   uint8_t* dest = (uint8_t*)bytes;
@@ -85,36 +85,45 @@ size_t neon_interleaved_despace(char *bytes, size_t howmany) {
      [ 00–07, 10–17, 20–27, 30–37, 40–47, 50–57, 60–67, 70–77 ]
      */
 
-    uint8x16x4_t characters = vld4q_u8(source);
+    uint8x8_t goodBits_doubles[2];
+    for (int i = 0; i != 2; ++i) {
+      uint8x16x4_t characters = vld4q_u8(source + 8 * 8 * i);
 
-    const uint16x8x4_t masks = {
-      vdupq_n_u16(0x8008), vdupq_n_u16(0x4004), vdupq_n_u16(0x2002), vdupq_n_u16(0x1001)
-    };
+      const uint16x8x4_t masks = {
+        vdupq_n_u16(0x8008), vdupq_n_u16(0x4004), vdupq_n_u16(0x2002), vdupq_n_u16(0x1001)
+      };
 
-    uint16x8_t goodBitsQuad = vdupq_n_u16(0);
-    for (int j = 0; j != 4; ++j) {
-      uint8x16_t good = vcgtq_u8(characters.val[j], vdupq_n_u8(space));
-      goodBitsQuad = vorrq_u16(goodBitsQuad, vandq_u16(masks.val[j], vreinterpretq_u16_u8(good)));
+      uint16x8_t goodBitsQuad = vdupq_n_u16(0);
+      for (int j = 0; j != 4; ++j) {
+        uint8x16_t good = vcgtq_u8(characters.val[j], vdupq_n_u8(space));
+        goodBitsQuad = vorrq_u16(goodBitsQuad, vandq_u16(masks.val[j], vreinterpretq_u16_u8(good)));
+      }
+
+      goodBits_doubles[i] = vshrn_n_u16(vreinterpretq_u16_u8(vrev16q_u8(vreinterpretq_u8_u16(goodBitsQuad))), 4);
     }
 
-    uint8x8_t goodBits = vshrn_n_u16(vreinterpretq_u16_u8(vrev16q_u8(vreinterpretq_u8_u16(goodBitsQuad))), 4);
+    uint8x16_t goodBits = vcombine_u8(goodBits_doubles[0], goodBits_doubles[1]);
 
-    uint8x8_t goodCount = vcnt_u8(goodBits);
+    uint8x16_t goodCount = vcntq_u8(goodBits);
 
-    uint8x8_t indices[8];
+    uint8x16_t indices[8];
     for (int i = 0; i != 8; ++i) {
-      uint8x8_t indicesOfSetBits = vclz_u8(goodBits);
+      uint8x16_t indicesOfSetBits = vclzq_u8(goodBits);
       indices[i] = indicesOfSetBits;
-      goodBits = vbic_u8(goodBits, vshl_u8(vdup_n_u8(0x80), vneg_s8(vreinterpret_s8_u8(indicesOfSetBits))));
+      goodBits = vbicq_u8(goodBits, vshlq_u8(vdupq_n_u8(0x80), vnegq_s8(vreinterpretq_s8_u8(indicesOfSetBits))));
     }
-    transpose_8x8(indices);
+    transpose_q_8x8(indices);
 
-    for (int i = 0; i != 8; ++i) {
-      uint8x8_t originalCharacters = vld1_u8(source + 8 * i);
-      uint8x8_t pickedCharacters = vtbl1_u8(originalCharacters, indices[i]);
-      vst1_u8(dest, pickedCharacters);
-      dest += vget_lane_u8(goodCount, 0);
-      goodCount = vext_u8(goodCount, goodCount, 1);
+    for (int i = 0; i != 2; ++i) {
+      uint8x8_t goodCount_i = i ? vget_high_u8(goodCount) : vget_low_u8(goodCount);
+      for (int j = 0; j != 8; ++j) {
+        uint8x8_t originalCharacters = vld1_u8(source + 8 * (8 * i + j));
+        uint8x8_t indices_ij = i ? vget_high_u8(indices[j]) : vget_low_u8(indices[j]);
+        uint8x8_t pickedCharacters = vtbl1_u8(originalCharacters, indices_ij);
+        vst1_u8(dest, pickedCharacters);
+        dest += vget_lane_u8(goodCount_i, 0);
+        goodCount_i = vext_u8(goodCount_i, goodCount_i, 1);
+      }
     }
 
     source += blockSize;
