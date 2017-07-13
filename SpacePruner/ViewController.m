0ba311fd6f13aa626cdcca6e689db2e267058c25
Derek Ledbetter <derek.ledbetter.0127@gmail.com>
2017-07-12 21:35:49 -0700

    Prints results to the screen.

--- SpacePruner/ViewController.m
+++ SpacePruner/ViewController.m
@@ -8,15 +8,30 @@
 #import "ViewController.h"
 
 #include "despacebenchmark.h"
+#include <stdio.h>
 
 @interface ViewController () {
-    BOOL _currentlyRunning; // TODO: change to property
+  BOOL _currentlyRunning; // TODO: change to property
+  NSMutableString *_logText;
 }
 
 @property (nonatomic, weak) IBOutlet UIButton *button;
 @property (nonatomic, weak) IBOutlet UILabel *label;
-
 @end
+
+
+@interface ViewController (Private)
+- (void)addToLogFromBytes:(const char*)bytes length:(int)length;
+@end
+
+int streamWriteFunction(void *cookie, const char *buf, int nbyte) {
+  __weak ViewController* weakController = (__bridge __weak ViewController*)cookie;
+  ViewController* controller = weakController;
+  if (controller) {
+    [controller addToLogFromBytes:buf length:nbyte];
+  }
+  return nbyte;
+}
 
 @implementation ViewController
 
@@ -34,6 +49,7 @@
 
 - (void)setUp {
   _currentlyRunning = NO;
+  _logText = [NSMutableString string];
 }
 
 - (void)viewDidLoad {
@@ -47,31 +63,39 @@
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)runTest:(id)sender {
-  // TODO
   if (_currentlyRunning) {
     return;
   }
 
   _currentlyRunning = YES;
   [self updateRunButton];
+  [_logText deleteCharactersInRange:(NSRange){ 0, _logText.length } ];
+
   __weak ViewController *weakSelf = self;
   dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^(void) {
-    // TODO: print results to screen.
-    despace_benchmark(stdout);
-    NSString* message = @"Printed benchmark results to log.\n";
+    void* cookie = (__bridge void*)weakSelf;
+    FILE* stream = fwopen(cookie, streamWriteFunction);
+    despace_benchmark(stream);
+    fclose(stream);
 
     ViewController* self = weakSelf;
     if (self) {
       dispatch_async(dispatch_get_main_queue(), ^(void) {
         self->_currentlyRunning = NO;
         [self updateRunButton];
-        self.label.text = message;
       });
     }
+  });
+}
+
+- (void)addToLogFromBytes:(const char*)bytes length:(int)length {
+  NSString* newString = [[NSString alloc] initWithBytes:bytes length:length encoding:NSASCIIStringEncoding];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [_logText appendString:newString];
+    self.label.text = _logText;
   });
 }
 
